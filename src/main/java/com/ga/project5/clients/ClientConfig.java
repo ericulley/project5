8efa15b5fb2872dba9ca630fc54a7ebc9962a5dc
Your Name <you@example.com>
2021-02-17 01:17:27 -0700

    heroku

--- src/main/java/com/ga/project5/clients/ClientConfig.java
+++ src/main/java/com/ga/project5/clients/ClientConfig.java
@@ -4,21 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
-
 @Configuration
 public class ClientConfig {
-
-    @Bean
-    CommandLineRunner commandLineRunner (ClientRepository repository){
-        return args -> {
-            Client bob = new Client(
-                    "bob",
-                    "bob@bob.com",
-                    "password"
-            );
-        };
-    }
 
 
 }
